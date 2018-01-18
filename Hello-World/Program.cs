972a0fb454d1d30154874d938a75360880e89e9c
shaileshs <shaileshs@SHAILESHS>
2018-01-18 11:07:49 +0530

    Add project files.

---
+++ Hello-World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome.....");
+            Console.ReadLine();
+        }
+    }
+}
